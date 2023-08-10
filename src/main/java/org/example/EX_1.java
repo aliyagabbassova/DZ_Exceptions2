655be6abc0cd1a353d68a50c9ab48e0a5bdb876e
aliya <aliya.gabbassova1911@gmail.com>
2023-08-10 10:08:27 +0600

    2nd homework

--- src/main/java/org/example/EX_1.java
+++ src/main/java/org/example/EX_1.java
@@ -1,2 +1,24 @@
-package org.example;public class EX_1 {
+package org.example;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class EX_1 {
+    public static void main(String[] args) {
+        float num = 0;
+        boolean flag = true;
+        do{
+            try{
+                Scanner in = new Scanner(System.in);
+                System.out.print("Введите число (дробное число вводится через запятую): ");
+                num = in.nextFloat();
+                flag = false;
+            }catch(InputMismatchException e){
+                System.out.println("Введённое значение не является числом, поробуйте снова.");
+            }
+        }while(flag);
+        System.out.println(num);
+    }
 }
+
+
--- src/main/java/org/example/EX_2and3.java
+++ src/main/java/org/example/EX_2and3.java
@@ -1,2 +1,43 @@
-package org.example;public class EX_2and3 {
-}
+package org.example;
+
+public class EX_2and3 {
+
+        public static void main(String[] args) {
+            foo1();
+            try {
+                int a = 90;
+                int b = 3;
+                System.out.println(a / b);
+                printSum(23, 234);
+                int[] abc = { 1, 2};
+                abc[3] = 9;
+
+            } catch (NullPointerException ex) {
+                System.out.println("Указатель не может указывать на null!");
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println("Массив выходит за пределы своего размера!");
+            } catch (Throwable ex) {
+                System.out.println("Что-то пошло не так...");
+            }
+
+        }
+        public static void printSum(Integer a, Integer b){
+            System.out.println(a + b);
+        }
+
+
+        // Если необходимо, исправьте данный код
+        public static void foo1(){
+            try {
+                int[] intArray = {1,2,3,4,5,6,7,8,9};
+                int d = 0;
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            } catch (ArithmeticException e) {
+                System.out.println("Catching exception: " + e);
+            }
+
+        }
+
+    }
+
